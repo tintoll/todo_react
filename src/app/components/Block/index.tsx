9e0b4ec34c901affc2f6117d8447aeac41e3c134
tintoll <belselios@gmail.com>
2022-09-27 21:56:35 +0900

    4-5 내용

---
+++ src/app/components/Block/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default styled.div<{
+  marginTop?: string;
+  marginLeft?: string;
+  marginBottom?: string;
+  marginRight?: string;
+}>`
+  margin-top: ${props => props.marginTop || '0'};
+  margin-bottom: ${props => props.marginBottom || '0'};
+  margin-left: ${props => props.marginLeft || '0'};
+  margin-right: ${props => props.marginRight || '0'};
+`;
--- src/app/pages/HomePage/index.tsx
+++ src/app/pages/HomePage/index.tsx
@@ -3,6 +3,7 @@ import { Helmet } from 'react-helmet-async';
 import styled from 'styled-components';
 
 import TodoInput from 'app/components/TodoInput';
+import TodoItem from 'app/components/TodoItem';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -26,17 +27,6 @@ const Title = styled.h1`
 `;
 
 const TodoList = styled.div``;
-const TodoItem = styled.div`
-  display: flex;
-  align-items: center;
-  padding: 15px 25px;
-  width: 100%;
-  font-size: 1.2em;
-  border-bottom: 1px solid #eee;
-`;
-const TodoCheck = styled.input`
-  margin-right: 15px;
-`;
 
 export function HomePage() {
   return (
@@ -50,10 +40,30 @@ export function HomePage() {
           <Title>할일</Title>
           <TodoInput />
           <TodoList>
-            <TodoItem>
-              <TodoCheck type="checkbox" />
-              나는 투두야
-            </TodoItem>
+            <TodoItem
+              todo={{
+                id: '1',
+                completed: true,
+                editing: true,
+                content: '할일 1',
+              }}
+            />
+            <TodoItem
+              todo={{
+                id: '2',
+                completed: false,
+                editing: false,
+                content: '할일 2',
+              }}
+            />
+            <TodoItem
+              todo={{
+                id: '3',
+                completed: false,
+                editing: false,
+                content: '할일 3',
+              }}
+            />
           </TodoList>
         </Box>
       </Wrapper>
